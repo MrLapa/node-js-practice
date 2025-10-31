fc72ffb6d748ae786082e9581b83a1a1664b9b4e
Carlos Alí García <mrgarcia7@gmail.com>
2025-10-31 17:38:46 -0500

    Create a local server with http library (#1)
    * Create a local server with http library
    
    * Keep initial file

--- 1-run-app-with-files/app.js
+++ 1-run-app-with-files/app.js
@@ -1,3 +1,19 @@
 const http = require("http");
 
 console.log("Hello, World!");
+
+const server = http.createServer((req, res) => {
+  // res.writeHead(200, { "Content-Type": "text/plain" });
+  // res.end("Hello, World!");
+  console.log("Received a header", req.headers);
+  console.log("Received a URL", req.url);
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(
+    "<html><head><title>Hello</title></head><body><h1>Hello, World!</h1></body></html>"
+  );
+  res.end();
+});
+
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
