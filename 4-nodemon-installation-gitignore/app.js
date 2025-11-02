3b0c54069e35dc2526046529858c475c9170e255
Carlos Garcia <mrgarcia7@gmail.com>
2025-11-02 14:26:53 -0500

    Add file to create server to test event loop

---
+++ 4-nodemon-installation-gitignore/app.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+console.log("Hello, World!");
+
+const server = http.createServer((req, res) => {
+  // res.writeHead(200, { "Content-Type": "text/plain" });
+  // res.end("Hello, World!");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(
+    "<html><head><title>Hello</title></head><body><h1>Hello, World! 2222</h1></body></html>"
+  );
+  res.end();
+});
+
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
