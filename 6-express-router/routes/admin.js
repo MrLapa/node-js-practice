1701fbf58a49e07f05e2224eee153c593dd7957c
Carlos Garcia <mrgarcia7@gmail.com>
2025-11-03 15:07:53 -0500

    Remove unused parameters

--- 6-express-router/routes/admin.js
+++ 6-express-router/routes/admin.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/dashboard', (req, res, next) => {
+router.get('/dashboard', (req, res) => {
   res.send('<form action="/admin/settings" method="POST"><input type="text" name="setting" placeholder="New Setting"><button type="submit">Go to Settings</button></form>');
 });
 
-router.post('/settings', (req, res, next) => {
+router.post('/settings', (req, res) => {
   res.send(`<h1>Admin Settings Page</h1><p>${req.body.setting}</p>`);
 });
 
