6fb8d187e5e3be8ab1ae91a2feb931a8817eeded
Carlos Garcia <mrgarcia7@gmail.com>
2025-11-03 14:58:33 -0500

    Implement routes

--- 6-express-router/app.js
+++ 6-express-router/app.js
@@ -2,17 +2,13 @@ const http = require("http");
 
 const express = require("express");
 
+const adminRoutes = require("./routes/admin");
+
 const app = express();
 
-app.use("/paragraph", (req, res, next) => {
-  console.log("Paragraph route accessed", req.url);
-  res.send("<div><h1>Hello, World! from Express</h1><p>This is a paragraph.</p></div>");
-});
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/", (req, res, next) => {
-  console.log("Root route accessed", req.url);
-  res.send("<h1>Hello, World! from Express</h1>");
-});
+app.use("/admin", adminRoutes);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
---
+++ 6-express-router/routes/admin.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/dashboard', (req, res, next) => {
+  res.send('<form action="/admin/settings" method="POST"><input type="text" name="setting" placeholder="New Setting"><button type="submit">Go to Settings</button></form>');
+});
+
+router.post('/settings', (req, res, next) => {
+  res.send(`<h1>Admin Settings Page</h1><p>${req.body.setting}</p>`);
+});
+
+module.exports = router;
