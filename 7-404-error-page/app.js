7fa5887c043c03bd9a385d7cf8aaa1178152c7a8
Carlos Garcia <mrgarcia7@gmail.com>
2025-11-03 21:53:18 -0500

    Implementing 404 error response

---
+++ 7-404-error-page/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const adminRoutes = require("./routes/admin");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/admin", adminRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
