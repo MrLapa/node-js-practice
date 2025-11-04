fe1e75455857b81930a0ee7d0920b90a1ecd1c08
Carlos Alí García <mrgarcia7@gmail.com>
2025-11-03 21:43:24 -0500

    6 express router (#5)
    * Initial files
    
    * Implement routes
    
    * Remove unused libraries and add app name and description in package
    
    * Remove unused parameters
    
    * Format code

---
+++ 6-express-router/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const adminRoutes = require("./routes/admin");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/admin", adminRoutes);
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
